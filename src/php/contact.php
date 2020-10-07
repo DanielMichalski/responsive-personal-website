b33cbc39d124aed5e407dd1afbee511ec4e73045
Daniel Michalski <michalskidaniel2@gmail.com>
2020-10-08 00:32:17 +0200

    Refactor

--- src/php/contact.php
+++ src/php/contact.php
@@ -53,10 +53,6 @@ $e_subject = 'You have been contacted by ' . $name . '.';
 // You can change this if you feel that you need to.
 // Developers, you may wish to add more fields to the form, in which case you must be sure to add them here.
 
-$e_body = "You have been contacted by $name. Their additional message is as follows." . PHP_EOL . PHP_EOL;
-$e_content = "\"$comments\"" . PHP_EOL . PHP_EOL;
-$e_reply = "You can contact $name via email, $email";
-
 $msg = wordwrap( $e_body . $e_content . $e_reply, 70 );
 
 $headers = "From: $email" . PHP_EOL;
@@ -65,7 +61,7 @@ $headers .= "MIME-Version: 1.0" . PHP_EOL;
 $headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
 $headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
 
-if(mail($address, $e_subject, $msg, $headers)) {
+if(mail($address, $e_subject, $comments, $headers)) {
 
 	// Email has sent successfully, echo a success page.
 
