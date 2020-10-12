97bf0550ce501b3facd0abc60b5baa9cdb052f15
Daniel Michalski <michalskidaniel2@gmail.com>
2020-10-12 15:33:33 +0200

    Refactor

--- www/js/typed.init.js
+++ www/js/typed.init.js
@@ -1,10 +1,9 @@
-
 // Typed Text
-$(".element").each(function(){
+$(".element").each(function () {
     var $this = $(this);
     $this.typed({
-    strings: $this.attr('data-elements').split(','),
-    typeSpeed: 100, // typing speed
-    backDelay: 3000 // pause before backspacing
+        strings: $this.attr('data-elements').split(','),
+        typeSpeed: 40,
+        backDelay: 1500
     });
 });
