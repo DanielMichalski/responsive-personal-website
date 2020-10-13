69c89839d62fad28a124baaf49e71ae7a9b82587
Daniel Michalski <michalskidaniel2@gmail.com>
2020-10-13 11:30:35 +0200

    Refactor

--- www/js/app/app.js
+++ www/js/app/app.js
@@ -64,7 +64,7 @@
     }); 
 
     $(".back-to-top").on("click", function() {
-        $("html, body").animate({ scrollTop: 0 }, 1000);
+        $("html, body").animate({ scrollTop: 0 }, 2500);
         return false;
     });
     
